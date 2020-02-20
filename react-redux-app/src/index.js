48787a9649b88a500f9d3500847da1b9c2d20730
unknown <eastkim1@hotmail.com>
2020-02-20 15:42:46 -0700

    Reducers

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -1,12 +1,17 @@
 import axios from "axios"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 
-
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
 export const FETCH_DATA = "FETCH_DATA";
 export const UPDATE_CHARACTER = "UPDATE_CHARACTER";
 export const SET_ERROR = "SET_ERROR";
 export const getData = () => dispatch => {
-
   dispatch({ type: FETCH_DATA });
   axios
     .get("https://rickandmortyapi.com/api/character/2")
