95f354ddbec08f73e9132c01b7e06f84a02d9f3e
unknown <eastkim1@hotmail.com>
2020-02-20 15:27:20 -0700

    Starting components

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+
+import { characterReducer as reducer } from "./reducers/characterReducer"
 import './App.css';
 
-function App() {
+import CharacterForm from "./components/CharacterForm"
+import CharacterDiv from "./components/CharacterDiv"
+
+const store = createStore(reducer, applyMiddleware(thunk))
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <CharacterForm />
+      <CharacterDiv />
+    </Provider>
   );
 }
-
-export default App;
